f29524671d60d192ac279f909bf27d265d72be40
kelvinlogic <kelvin6600@gmail.com>
2015-12-07 08:49:49 +0300

    Initial commit

---
+++ src/com/polyrational/controller/UserController.java
@@ -0,0 +1,78 @@
+package com.polyrational.controller;
+
+import com.polyrational.database.UserDB;
+import com.polyrational.model.User;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class UserController extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+    private static String INSERT_OR_EDIT = "/user.jsp";
+    private static String LIST_USER = "/listUser.jsp";
+    private UserDB dbObject;
+
+    public UserController() {
+        super();
+        dbObject = new UserDB();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String forward="";
+        String action = request.getParameter("action");
+
+        if (action.equalsIgnoreCase("delete")){
+            int userId = Integer.parseInt(request.getParameter("userId"));
+            dbObject.deleteUser(userId);
+            forward = LIST_USER;
+            request.setAttribute("users", dbObject.getAllUsers());
+        } else if (action.equalsIgnoreCase("edit")){
+            forward = INSERT_OR_EDIT;
+            int userId = Integer.parseInt(request.getParameter("userId"));
+            User user = dbObject.getUserById(userId);
+            request.setAttribute("user", user);
+        } else if (action.equalsIgnoreCase("listUser")){
+            forward = LIST_USER;
+            request.setAttribute("users", dbObject.getAllUsers());
+        } else {
+            forward = INSERT_OR_EDIT;
+        }
+
+        RequestDispatcher view = request.getRequestDispatcher(forward);
+        view.forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        User user = new User();
+        user.setFirstName(request.getParameter("firstName"));
+        user.setLastName(request.getParameter("lastName"));
+        try {
+            Date dob = new SimpleDateFormat("MM/dd/yyyy").parse(request.getParameter("dob"));
+            user.setDob(dob);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        user.setEmail(request.getParameter("email"));
+        String userid = request.getParameter("userid");
+        if(userid == null || userid.isEmpty())
+        {
+            dbObject.addUser(user);
+        }
+        else
+        {
+            user.setUserid(Integer.parseInt(userid));
+            dbObject.updateUser(user);
+        }
+        RequestDispatcher view = request.getRequestDispatcher(LIST_USER);
+        request.setAttribute("users", dbObject.getAllUsers());
+        view.forward(request, response);
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: kelvin
+  Date: 12/7/2015
+  Time: 7:51 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>Simple CRUD</title>
+  </head>
+  <body>
+  <jsp:forward page="/UserController?action=listUser" />
+  </body>
+</html>
